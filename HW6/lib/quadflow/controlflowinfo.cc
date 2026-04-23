99d8add4fa44ca41643d1accd1c07f94b9113d18
Starw1sh <23307110269@m.fudan.edu.cn>
2026-04-23 17:36:40 +0800

    hw6 run

--- HW6/lib/quadflow/controlflowinfo.cc
+++ HW6/lib/quadflow/controlflowinfo.cc
@@ -49,6 +49,27 @@ void ControlFlowInfo::computeUnreachableBlocks() {
     cout << "Computing unreachable blocks in function: " << func->funcname << endl;
 #endif
     // FILE IN THE CODE HERE TO COMPUTE UNREACHABLE BLOCKS
+    // 使用BFS/DFS从入口块遍历所有可达块，剩下的就是不可达块
+    unreachableBlocks.clear();
+    if (entryBlock == -1) return;
+    set<int> visited;
+    queue<int> q;
+    q.push(entryBlock);
+    visited.insert(entryBlock);
+    while (!q.empty()) {
+        int cur = q.front(); q.pop();
+        if (successors.count(cur)) {
+            for (int succ : successors[cur]) {
+                if (!visited.count(succ)) {
+                    visited.insert(succ);
+                    q.push(succ);
+                }
+            }
+        }
+    }
+    for (int b : allBlocks) {
+        if (!visited.count(b)) unreachableBlocks.insert(b);
+    }
 }
 
 void ControlFlowInfo::eliminateUnreachableBlocks() {
@@ -57,16 +78,60 @@ void ControlFlowInfo::eliminateUnreachableBlocks() {
 #endif
     // FILE IN THE CODE HERE TO ELIMINATE UNREACHABLE BLOCKS from the function,
     // and emptying all the sets and maps above since everything may need to be recomputed!
+    // 先移除不可达块
+    if (unreachableBlocks.empty()) return;
+    // 移除func->quadblocklist中对应的block
+    auto &blocks = *func->quadblocklist;
+    blocks.erase(
+        remove_if(blocks.begin(), blocks.end(), [&](quad::QuadBlock* blk) {
+            return blk && blk->entry_label && unreachableBlocks.count(blk->entry_label->num);
+        }),
+        blocks.end()
+    );
+    // 清空所有分析结果
+    allBlocks.clear();
+    labelToBlock.clear();
+    predecessors.clear();
+    successors.clear();
+    dominators.clear();
+    immediateDominator.clear();
+    dominanceFrontiers.clear();
+    domTree.clear();
+    unreachableBlocks.clear();
 }
 
 void ControlFlowInfo::computePredecessors() {
     // Compute predecessors for each block
     // FILE IN THE CODE HERE TO COMPUTE PREDECESSORS for each block
+    predecessors.clear();
+    for (int b : allBlocks) predecessors[b] = set<int>();
+    for (int b : allBlocks) {
+        if (!labelToBlock.count(b)) continue;
+        quad::QuadBlock* blk = labelToBlock[b];
+        if (!blk->exit_labels) continue;
+        for (auto lbl : *blk->exit_labels) {
+            if (lbl && allBlocks.count(lbl->num)) {
+                predecessors[lbl->num].insert(b);
+            }
+        }
+    }
 }
 
 void ControlFlowInfo::computeSuccessors() {
     // Compute successors for each block
     // FILE IN THE CODE HERE TO COMPUTE SUCCESSORS for each block
+    successors.clear();
+    for (int b : allBlocks) successors[b] = set<int>();
+    for (int b : allBlocks) {
+        if (!labelToBlock.count(b)) continue;
+        quad::QuadBlock* blk = labelToBlock[b];
+        if (!blk->exit_labels) continue;
+        for (auto lbl : *blk->exit_labels) {
+            if (lbl && allBlocks.count(lbl->num)) {
+                successors[b].insert(lbl->num);
+            }
+        }
+    }
 }
 
 void ControlFlowInfo::computeDominators() {
@@ -75,6 +140,30 @@ void ControlFlowInfo::computeDominators() {
 #endif
     // Compute dominators for each block
     // FILE IN THE CODE HERE TO COMPUTE DOMINATORS for each block
+    // 经典迭代算法
+    dominators.clear();
+    for (int b : allBlocks) {
+        if (b == entryBlock) dominators[b] = {b};
+        else dominators[b] = allBlocks;
+    }
+    bool changed = true;
+    while (changed) {
+        changed = false;
+        for (int b : allBlocks) {
+            if (b == entryBlock) continue;
+            set<int> newDom = allBlocks;
+            for (int p : predecessors[b]) {
+                set<int> tmp;
+                set_intersection(newDom.begin(), newDom.end(), dominators[p].begin(), dominators[p].end(), inserter(tmp, tmp.begin()));
+                newDom = tmp;
+            }
+            newDom.insert(b);
+            if (newDom != dominators[b]) {
+                dominators[b] = newDom;
+                changed = true;
+            }
+        }
+    }
 }
 
 void ControlFlowInfo::computeImmediateDominator() {
@@ -82,6 +171,25 @@ void ControlFlowInfo::computeImmediateDominator() {
     std::cout << "Start to find immediate dominators for: " << func->funcname << endl;
 #endif
     //FILE IN THE CODE HERE TO COMPUTE IMMEDIATE DOMINATOR for each block, using the dominators information computed above
+    immediateDominator.clear();
+    for (int b : allBlocks) {
+        if (b == entryBlock) continue;
+        set<int> doms = dominators[b];
+        doms.erase(b);
+        int idom = -1;
+        for (int d : doms) {
+            bool isImm = true;
+            for (int d2 : doms) {
+                if (d == d2) continue;
+                if (dominators[d2].count(d)) {
+                    isImm = false;
+                    break;
+                }
+            }
+            if (isImm) { idom = d; break; }
+        }
+        if (idom != -1) immediateDominator[b] = idom;
+    }
 }
 
 void ControlFlowInfo::computeDomTree() {
@@ -89,13 +197,38 @@ void ControlFlowInfo::computeDomTree() {
         std::cout << "Computing dominator tree for: " << func->funcname << endl;
     #endif
     // FILE IN THE CODE HERE TO COMPUTE DOMINATOR TREE using immediate dominators
+    domTree.clear();
+    for (int b : allBlocks) {
+        if (immediateDominator.count(b)) {
+            int idom = immediateDominator[b];
+            domTree[idom].insert(b);
+        }
+    }
 }
 
 void ControlFlowInfo::computeDominanceFrontiers() {
 #ifdef DEBUG
     std::cout << "Computing dominance frontier for: " << func->funcname << endl;
 #endif
-
     //FILE IN THE CODE HERE TO COMPUTE DOMINANCE FRONTIER for each block, using the successors, dominators, immediate dominator, and domTree information computed above
+
+    dominanceFrontiers.clear();
+    for (int b : allBlocks) {
+        set<int> df;
+        if (!successors.count(b)) continue;
+        for (int s : successors[b]) {
+            if (immediateDominator.count(s) && immediateDominator[s] != b) {
+                df.insert(s);
+            }
+        }
+        for (int c : domTree[b]) {
+            for (int w : dominanceFrontiers[c]) {
+                if (!dominators[b].count(w) || b == w) {
+                    df.insert(w);
+                }
+            }
+        }
+        dominanceFrontiers[b] = df;
+    }
 
 }
--- HW6/lib/quadflow/dataflowinfo.cc
+++ HW6/lib/quadflow/dataflowinfo.cc
@@ -17,6 +17,31 @@ void DataFlowInfo::findAllVars() {
 #endif
     //FILE IN THE CODE HERE TO FIND ALL VARIABLES used or defined in the function,
     //and filling in the allVars set, defs map and uses map 
+    allVars.clear();
+    defs->clear();
+    uses->clear();
+    if (!func || !func->quadblocklist) return;
+    for (auto block : *func->quadblocklist) {
+        if (!block || !block->quadlist) continue;
+        for (auto stm : *block->quadlist) {
+            if (!stm) continue;
+            // def/use 是 Temp* 集合
+            if (stm->def) {
+                for (auto t : *stm->def) {
+                    if (!t) continue;
+                    allVars.insert(t->num);
+                    (*defs)[t->num].insert({block, stm});
+                }
+            }
+            if (stm->use) {
+                for (auto t : *stm->use) {
+                    if (!t) continue;
+                    allVars.insert(t->num);
+                    (*uses)[t->num].insert({block, stm});
+                }
+            }
+        }
+    }
 }
 
 // Calculate both live-in and live-out sets for all statements
@@ -25,6 +50,84 @@ void DataFlowInfo::computeLiveness() {
     cout << "Computing liveness for function: " << func->funcname << endl;
 #endif
     //FILE IN THE CODE HERE TO CALCULATE BOTH LIVE-IN AND LIVE-OUT SETS for all statements in the function,
+    livein->clear();
+    liveout->clear();
+    if (!func || !func->quadblocklist) return;
+    // 收集所有语句
+    vector<quad::QuadStm*> stmts;
+    map<quad::QuadStm*, set<quad::QuadStm*>> succs;
+    map<quad::QuadStm*, set<int>> defmap, usemap;
+    map<quad::QuadStm*, quad::QuadBlock*> stmt2block;
+    for (auto block : *func->quadblocklist) {
+        if (!block || !block->quadlist) continue;
+        for (size_t i = 0; i < block->quadlist->size(); ++i) {
+            quad::QuadStm* stm = block->quadlist->at(i);
+            if (!stm) continue;
+            stmts.push_back(stm);
+            stmt2block[stm] = block;
+            if (stm->def) {
+                set<int> defnums;
+                for (auto t : *stm->def) if (t) defnums.insert(t->num);
+                defmap[stm] = defnums;
+            }
+            if (stm->use) {
+                set<int> usenums;
+                for (auto t : *stm->use) if (t) usenums.insert(t->num);
+                usemap[stm] = usenums;
+            }
+        }
+    }
+    // 构建语句级后继关系
+    for (auto block : *func->quadblocklist) {
+        if (!block || !block->quadlist || block->quadlist->empty()) continue;
+        for (size_t i = 0; i < block->quadlist->size(); ++i) {
+            quad::QuadStm* stm = block->quadlist->at(i);
+            if (!stm) continue;
+            set<quad::QuadStm*> sset;
+            if (i + 1 < block->quadlist->size()) {
+                sset.insert(block->quadlist->at(i + 1));
+            } else {
+                // 最后一条语句，后继是本块所有出口块的第一条语句
+                if (block->exit_labels) {
+                    for (auto lbl : *block->exit_labels) {
+                        for (auto b2 : *func->quadblocklist) {
+                            if (b2 && b2->entry_label && lbl && b2->entry_label->num == lbl->num && b2->quadlist && !b2->quadlist->empty()) {
+                                sset.insert(b2->quadlist->at(0));
+                            }
+                        }
+                    }
+                }
+            }
+            succs[stm] = sset;
+        }
+    }
+    // 迭代求解
+    map<quad::QuadStm*, set<int>> in, out;
+    bool changed = true;
+    while (changed) {
+        changed = false;
+        for (auto stm : stmts) {
+            set<int> old_in = in[stm], old_out = out[stm];
+            // out = 所有后继的 in
+            set<int> new_out;
+            for (auto s : succs[stm]) {
+                new_out.insert(in[s].begin(), in[s].end());
+            }
+            out[stm] = new_out;
+            // in = use ∪ (out - def)
+            set<int> new_in = usemap[stm];
+            set<int> out_minus_def;
+            set_difference(out[stm].begin(), out[stm].end(), defmap[stm].begin(), defmap[stm].end(), inserter(out_minus_def, out_minus_def.begin()));
+            new_in.insert(out_minus_def.begin(), out_minus_def.end());
+            in[stm] = new_in;
+            if (in[stm] != old_in || out[stm] != old_out) changed = true;
+        }
+    }
+    // 写回
+    for (auto stm : stmts) {
+        (*livein)[stm] = in[stm];
+        (*liveout)[stm] = out[stm];
+    }
 }
 
 set<DataFlowInfo*>* dataFLowProg(QuadProgram* prog) {
